fc8f6691398c8e0e1b4fcf780fcc71bec7c482eb
ttsutchi <tailegog@gmail.com>
2018-10-05 17:20:39 +0900

    [Add][Update] add english comments and change names of some methods

--- RealmSample/AddViewController.swift
+++ RealmSample/AddViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 
 // 追加画面と更新画面を区別するためのデータ
+// Data for distinguishing between the add view and the update view.
 enum ViewType {
     case add
     case update
@@ -25,22 +26,28 @@ class AddViewController: UIViewController, UITextFieldDelegate {
 
     var viewType: ViewType = .add
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    
+    // 画面表示時の動作を設定する
+    // Set the operation when displaying the view.
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
         // 更新画面として使うときは、textFieldに内容を表示し、buttonのタイトルを変更する
+        // When using as update view, display the contents in textField and change the title of button.
         if viewType == .update {
             textField.text = task.value
             button.setTitle("Update", for: .normal)
         }
     }
 
-
+    
     // taskを追加する
+    // Add task.
     func addTask(value: String) {
         let task = Task(value: value)
         task.add()
@@ -48,81 +55,99 @@ class AddViewController: UIViewController, UITextFieldDelegate {
 
 
     // taskを更新する
+    // Update task.
     func updateTask(value: String) {
         task.update(task: task, value: value)
     }
 
 
     // リスト画面に遷移する
-    func goToListView() {
+    // Transit to list view.
+    func transitToListView() {
         self.navigationController?.popViewController(animated: true)
     }
     
     
     // textFieldが未入力の際にアラートを表示する
+    // Display alert when textField is empty.
     func presentAlert() {
         
         // アラートのコントローラ
+        // Controller of Alert.
         let alertController = UIAlertController(title: "The name of your task is empty.", message: "Please input the name of your task.", preferredStyle: .alert)
         
         // OKボタン
+        // OK button.
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         
-        // アラートにOKボタンを追加する
+        // Add OK button to alertController.
         alertController.addAction(okAction)
         
         // アラートを表示する
+        // Display the alert.
         present(alertController, animated: true, completion: nil)
     }
 
 
     // 追加(更新)ボタンをタップしたときの動作を設定する
+    // Set the action when tapping the add (update) button.
     @IBAction func didSelectAddButton() {
 
-        // textFieldが空の場合はアラートを表示する
+        // textFieldが未入力の際にアラートを表示する
+        // Display alert when textField is empty.
         guard let value = textField.text, !value.isEmpty else {
             
             // アラートを表示する
+            // Display alert.
             presentAlert()
             
             return
         }
 
         // 追加か更新かで処理を分ける
+        // Change processing by add or update.
         switch viewType {
             case .add:
                 // taskの追加を行う
+                // Add task.
                 self.addTask(value: value)
             
             case .update:
                 // taskの更新を行う
+                // Update task.
                 self.updateTask(value: value)
         }
 
         // リスト画面に遷移する
-        self.goToListView()
+        // Transit to list view.
+        self.transitToListView()
     }
     
 
     // 戻るボタンをタップしたときの動作を設定する
+    // Set action when tapping back button.
     @IBAction func didSelectBackButton() {
 
         // リスト画面に遷移する
-        self.goToListView()
+        // Transit to list view.
+        self.transitToListView()
     }
 
     
-    // キーボードのReturnキーでキーボードを閉じる
+    // Returnキーでキーボードを閉じる
+    // Close the keyboard with Return key.
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         // textFieldからフォーカスを外しキーボードを閉じる
+        // Focus off from textField and close keyboard.
         textField.resignFirstResponder()
 
         return true
     }
 
 
-    //画面がタップされたらキーボードを閉じる
+    // 画面がタップされたらキーボードを閉じる
+    // Close the keyboard when the screen is tapped/
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if(textField.isFirstResponder) {
             textField.resignFirstResponder()
--- RealmSample/ListViewController.swift
+++ RealmSample/ListViewController.swift
@@ -20,21 +20,25 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
         super.viewDidLoad()
 
         // TableViewのdataSourceとdelegateのメソッドをListViewControllerで呼ぶようにする
+        // Make TableView dataSource and delegate methods called in ListViewController.
         table.dataSource = self
         table.delegate = self
     }
 
 
     // 画面表示時の動作を設定する
+    // Set the operation when displaying the view.
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
         // 画面表示時にRealmからデータを読み込みTableViewに表示する
+        // Read data from Realm and display it on TableView when displaying the view.
         self.read()
     }
 
 
-    // Realmからのデータを読み込みとTableViewの更新を行う
+    // Realmからのデータにの読み込みとTableViewの更新を行う
+    // Read data from Realm and update TableView.
     func read() {
         tasks = task.readAll()
         table.reloadData()
@@ -42,16 +46,19 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
 
     // 画面遷移時の設定をする
+    // Make settings for view transition.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let addViewController = segue.destination as! AddViewController
 
-        // 追加か更新かで遷移時の処理を分ける
+        // 追加か更新かで遷移時の処理を変える
+        // Change processing at transition by add or update.
         switch segue.identifier {
         case "add":
             addViewController.viewType = .add
 
         case "update":
             // タップされたセルのデータを取得し更新画面に受け渡す
+            // Acquires the data of the tapped cell and passes it to the update view.
             let task = tasks[(table.indexPathForSelectedRow?.row)!]
 
             addViewController.viewType = .update
@@ -64,36 +71,43 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
 
     // 追加画面に遷移する
-    func goToAddView() {
+    // Transit to add view.
+    func transitToAddView() {
         self.performSegue(withIdentifier: "add", sender: nil)
     }
 
 
     // 更新画面に遷移する
-    func goToUpdateView() {
+    // Transit to update view.
+    func transitToUpdateView() {
         self.performSegue(withIdentifier: "update", sender: nil)
     }
 
 
     // 追加(+)ボタンをタップしたときの動作を設定する
+    // Set action when tapping add(+) button.
     @IBAction func didSelectPlusButton() {
 
         // 追加画面に遷移する
-        self.goToAddView()
+        // Transit to add view.
+        self.transitToAddView()
     }
 
 
     // TableViewのセル数を設定する
+    // Set the number of cells in TableView.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
     }
 
 
     // セルに表示する内容を設定する
+    // Set contents to display in cell.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
 
         // taskの内容をセルに表示する
+        // Display contents of task in cell.
         cell?.textLabel?.text = tasks[indexPath.row].value
 
         return cell!
@@ -101,25 +115,32 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
 
     // セルを編集可能にする
+    // Make the cell editable.
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
 
 
     // セルをスワイプしたときの設定をする
+    // Make settings when swiping cells.
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
         // 削除ボタンを追加する
+        // Add delete button.
         let delete: UITableViewRowAction = UITableViewRowAction(style: .destructive, title: "delete", handler: {(action, indexPath) in
 
             // スワイプされたセルのtaskを取得する
+            // Get the task of the swiped cell.
             let task = self.tasks[indexPath.row]
 
             // taskを削除する
+            // Delete the task.
             try! Task.realm.write {
                 Task.realm.delete(task)
             }
 
             // TableViewを更新する
+            // Update the TableView.
             self.read()
         })
 
@@ -128,9 +149,11 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
 
     // セルをタップしたときの動作を設定する
+    // Set the behavior when tapping a cell.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
         // 更新画面に遷移する
-        self.goToUpdateView()
+        // Transit to update view.
+        self.transitToUpdateView()
     }
 }
